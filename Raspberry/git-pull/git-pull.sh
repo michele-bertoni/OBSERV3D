715ff5033bc3f64d6ab0636d3dac2107e1963eab
michele-bertoni <michele2.bertoni@mail.polimi.it>
2020-08-09 23:36:35 +0200

    Updated git-pull and switch-off scripts

--- Raspberry/git-pull/git-pull.sh
+++ Raspberry/git-pull/git-pull.sh
@@ -9,47 +9,51 @@
 # All output goes to stdout and log file1
 #
 # Redirect stdout to a named pipe running tee with append
-#!exec > >(tee -a /home/pi/Printy-McPrintface/Raspberry/git-pull/log.txt)
+exec > >(tee /home/pi/Printy-McPrintface/Raspberry/git-pull/log.txt)
 
 # Redirect stderr to stdout
 exec 2>&1
 
+# Verbose mode
+set -x
+
 # Go to home directory
 cd /home/pi
 
-cloned = 0
+cloned=0
+sleepTime=0
 # If ~/Printy-McPrintface directory does not exist, clone the repository
-while [ ! -d "/home/pi/Printy-McPrintface" ] 
+while [ ! -d "/home/pi/Printy-McPrintface" ]
 do
-    git clone https://github.com/michele-bertoni/Printy-McPrintface
-    cloned = 1
+	sleep $sleepTime
+	git clone https://github.com/michele-bertoni/Printy-McPrintface
+   	cloned=1
+   	sleepTime=$(($sleepTime+1))
 done
 
 # Move to repository root directory and pull changes
 cd Printy-McPrintface/
-pulled = -1
-sleepTime = 1
-while [ $cloned==0 ] && [ $pulled<0 ]
+sleepTime=1
+while [ "$cloned" -eq 0 ]
 do
 	pull=`git pull`
-	if [[ $pull == *"Already up-to-date."* ]]; then
-		echo Already up-to-date.
-		pulled = 0
-	elif [[ $pull == *"file changed,"* ]]; then
-		echo Pulled successfully
-		pulled = 1
-	elif [[ $pull == *"fatal"* ]]; then
-		echo Unable to connect to GitHub, retry
-		sleep 1
-	else; then
-		echo Unknown error
+	if [[ "$?" -eq 0 ]]; then
+		if [[ "$pull" =~ "up to date" ]]; then
+			echo No changes
+			cloned=-1
+		else
+			echo Updated
+			cloned=1
+	fi
+	else
 		sleep $sleepTime
-		sleepTime = $sleepTime*2
+		sleepTime=$(($sleepTime*2))
 	fi
 done
 
-if [pulled == 1] || [cloned == 1]; then
+if [ "$cloned" -eq 1 ] || [ "`cat /home/pi/Printy-McPrintface/Raspberry/git-pull/lastUpload.txt`" -ne "0" ]; then
 	cd /home/pi
 	arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328old Printy-McPrintface/Arduino/printy.ino
-	arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328old Printy-McPrintface/Arduino/printy.ino  
+	arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328old Printy-McPrintface/Arduino/printy.ino
+	echo "$?" > /home/pi/Printy-McPrintface/Raspberry/git-pull/lastUpload.txt
 fi
