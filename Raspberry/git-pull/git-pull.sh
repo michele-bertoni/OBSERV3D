092f6e56a01ea13a1f718f355f2dd8ad2afeb9b6
michele-bertoni <michele2.bertoni@mail.polimi.it>
2020-08-10 19:00:28 +0200

    Added to git-pull serial synchronization, log.old and solved possible bugs in case of sudden Raspberry Pi power leak

--- Raspberry/git-pull/git-pull.sh
+++ Raspberry/git-pull/git-pull.sh
@@ -6,6 +6,9 @@
 #
 # @reboot /home/pi/Printy-McPrintface/Raspberry/git-pull/git-pull.sh
 #
+# Move last log to log.old
+mv /home/pi/Printy-McPrintface/Raspberry/git-pull/log.txt /home/pi/Printy-McPrintface/Raspberry/git-pull/log.old.txt
+
 # All output goes to stdout and log file1
 #
 # Redirect stdout to a named pipe running tee with append
@@ -17,13 +20,25 @@ exec 2>&1
 # Verbose mode
 set -x
 
+# Reset possibly leftover locks
+rm /home/pi/Printy-McPrintface/Raspberry/git-pull/.updating
+
+# If argv -f is present force compile and upload
+for i in "$@"
+do
+	if [ "$i" -eq "-f" ]; then
+		rm /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile
+		rm /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload
+	fi
+done
+
 # Go to home directory
 cd /home/pi
 
 cloned=0
 sleepTime=0
 # If ~/Printy-McPrintface directory does not exist, clone the repository
-while [ ! -d "/home/pi/Printy-McPrintface" ]
+while [ ! -d "/home/pi/Printy-McPrintface/.git" ]
 do
 	sleep $sleepTime
 	git clone https://github.com/michele-bertoni/Printy-McPrintface
@@ -33,7 +48,7 @@ done
 
 # Move to repository root directory and pull changes
 cd Printy-McPrintface/
-sleepTime=1
+sleepTime=0
 while [ "$cloned" -eq 0 ]
 do
 	pull=`git pull`
@@ -50,8 +65,12 @@ do
 	fi
 	# Otherwise retry
 	else
-		sleep $sleepTime
-		sleepTime=$(($sleepTime*2))
+		sleep 2
+		sleepTime=$(($sleepTime+2))
+		if [[ "$sleepTime" -gt 60 ]]; then
+			echo Repository unreachable
+			cloned=-1
+		fi
 	fi
 done
 
@@ -59,22 +78,32 @@ cd /home/pi
 compiled=-1
 
 # Compile if a new version is available, if it's the first time the code is compiled or if last compile was unsuccessfull
-if [ ! -f "/home/pi/Printy-McPrintface/Raspberry/git-pull/lastCompile.txt" ]; then
-	touch /home/pi/Printy-McPrintface/Raspberry/git-pull/lastCompile.txt
-	echo "1" > /home/pi/Printy-McPrintface/Raspberry/git-pull/lastCompile.txt
+if [ ! -f "/home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile" ]; then
+	touch /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile
+	echo "2" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile
 fi
-if [ "$cloned" -eq 1 ] || [ "`cat /home/pi/Printy-McPrintface/Raspberry/git-pull/lastCompile.txt`" -ne "0" ]; then
+if [ "$cloned" -eq 1 ] || [ "`cat /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile`" -ne "0" ]; then
+	echo "3" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile
 	/home/pi/bin/arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328old Printy-McPrintface/Arduino/printy.ino
-	echo "$?" > /home/pi/Printy-McPrintface/Raspberry/git-pull/lastCompile.txt
+	echo "$?" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile
 	compiled=$?
 fi
 
 # Upload if a new version is available, if it's the first time the code is uploaded or if last upload was unsuccessfull
-if [ ! -f "/home/pi/Printy-McPrintface/Raspberry/git-pull/lastUpload.txt" ]; then
-        touch /home/pi/Printy-McPrintface/Raspberry/git-pull/lastUpload.txt
-        echo "1" > /home/pi/Printy-McPrintface/Raspberry/git-pull/lastUpload.txt
+if [ ! -f "/home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload" ]; then
+        touch /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload
+        echo "2" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload
 fi
-if [ "$compiled" -eq 0 ] || [ "`cat /home/pi/Printy-McPrintface/Raspberry/git-pull/lastUpload.txt`" -ne "0" ]; then
+if [ "$compiled" -eq 0 ] || [ "`cat /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload`" -ne "0" ]; then
+	echo "3" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload
+	sleepTime=0
+	touch /home/pi/Printy-McPrintface/Raspberry/git-pull/.updating
+	while [ -f "/home/pi/Printy-McPrintface/Raspberry/controller-ino/.serialOpen" ] && [ "$sleepTime" -lt 60 ]
+	do
+		sleep 0.1
+		sleepTime=$(($sleepTime+0.1))
+	done
 	/home/pi/bin/arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328old Printy-McPrintface/Arduino/printy.ino
-	echo "$?" > /home/pi/Printy-McPrintface/Raspberry/git-pull/lastUpload.txt
+	echo "$?" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload
+	rm /home/pi/Printy-McPrintface/Raspberry/git-pull/.updating
 fi
