0f72185d4c2412ab0c9f1a0816de6d09c4ba7231
Michele Bertoni <michele2.bertoni@mail.polimi.it>
2020-08-11 02:39:21 +0200

    Update controller-ino.sh
    Added LICENSE

--- Raspberry/controller-ino/controller-ino.sh
+++ Raspberry/controller-ino/controller-ino.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #
+# Copyright (C) 2020 Michele Bertoni - All Rights Reserved
+# You may use, distribute and modify this code under the terms of the CC BY-NC-SA 3.0 license.
+# You can find a copy of the license inside the LICENSE file you received with this code 
+# (https://github.com/michele-bertoni/Printy-McPrintface/blob/master/LICENSE)
+# or on the website of CreativeCommons (https://creativecommons.org/licenses/by-nc-sa/3.0/)
+#
 # /home/pi/Printy-McPrintface/Raspberry/controller-ino
 #
 # This script is added to crontab (with crontab -e) as follows:
