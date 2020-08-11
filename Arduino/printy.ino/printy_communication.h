5a4ec15c4de8f1f6708b9c52a7d66cb3aa9c1331
michele-bertoni <michele2.bertoni@mail.polimi.it>
2020-08-11 03:31:03 +0200

    Added LICENSE headers to printy.ino source code

--- Arduino/printy.ino/printy_communication.h
+++ Arduino/printy.ino/printy_communication.h
@@ -1,3 +1,11 @@
+/***************************************************************************************************
+ * Copyright (C) 2020 Michele Bertoni - All Rights Reserved                                        *
+ * You may use, distribute and modify this code under the terms of the CC BY-NC-SA 3.0 license.    *
+ * You can find a copy of the license inside the LICENSE file you received with this code          *
+ * (https://github.com/michele-bertoni/Printy-McPrintface/blob/master/LICENSE)                     *
+ * or on the website of CreativeCommons (https://creativecommons.org/licenses/by-nc-sa/3.0/)       *
+ ***************************************************************************************************/
+
 #ifndef PRINTY_COMMUNICATION_H
 #define PRINTY_COMMUNICATION_H
 
