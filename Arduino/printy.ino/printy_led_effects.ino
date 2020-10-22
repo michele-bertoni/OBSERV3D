77fc5990f97452c1009e40282443b00a6aa7d366
michele-bertoni <michele2.bertoni@mail.polimi.it>
2020-10-23 01:12:25 +0200

    Solved minor bug with leds

--- Arduino/printy.ino/printy_led_effects.ino
+++ Arduino/printy.ino/printy_led_effects.ino
@@ -43,8 +43,8 @@ void LedEffects::handleLeds() {
     lastUpdateTime = time;
     //fill_solid(chLeds, NUM_CHLEDS, CHSV(ledManager.getHue(), ledManager.getSaturation(), ledManager.getBrightness()));
     //fill_solid(extLeds, NUM_EXTLEDS, CHSV(ledManager.getExtHue(), ledManager.getExtSaturation(), ledManager.getExtBrightness()));
-    fill_solid(chLeds, NUM_CHLEDS, CHSV(0, 0, 255);
-    fill_solid(extLeds, NUM_EXTLEDS, CHSV(0, 0, 128);
+    fill_solid(chLeds, NUM_CHLEDS, CHSV(0, 0, 255));
+    fill_solid(extLeds, NUM_EXTLEDS, CHSV(0, 0, 128));
     FastLED.show();
   }
 }
