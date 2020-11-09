b5347bf53b15bb6cf1858e02eaf3997025623ee0
michele-bertoni <michele2.bertoni@mail.polimi.it>
2020-11-09 16:40:00 +0100

    Updated git-pull for forcing reboot if one or more Raspberry scripts are edited

--- Raspberry/git-pull/git-pull.sh
+++ Raspberry/git-pull/git-pull.sh
@@ -55,19 +55,22 @@ cd /home/pi
 
 cloned=0
 sleepTime=0
+needReboot=0
+
 # If ~/Printy-McPrintface directory does not exist, clone the repository
-while [ ! -d "/home/pi/Printy-McPrintface/.git" ]
+while [[ ! -d "/home/pi/Printy-McPrintface/.git" ]]
 do
-	sleep $sleepTime
+	sleep ${sleepTime}
 	git clone https://github.com/michele-bertoni/Printy-McPrintface
-   	cloned=1
+   	cloned=3
+   	needReboot=1
    	sleepTime=$(($sleepTime+1))
 done
 
 # Move to repository root directory and pull changes
 cd Printy-McPrintface/
 sleepTime=0
-while [ "$cloned" -eq 0 ]
+while [[ "$cloned" -eq 0 ]]
 do
 	pull=`git pull`
 	# If pull was successfull
@@ -76,13 +79,21 @@ do
 		if [[ "$pull" =~ "up to date" ]]; then
 			echo No changes
 			cloned=-1
-		elif [[ "$pull" =~ "Arduino/printy.ino/" ]]; then
-                        echo Arduino source code changed
-                        cloned=1
-		# Otherwise if some non-Arduino files changed
+		# If pull changed Arduino files
 		else
-			echo Updated non-Arduino files
-			cloned=-2
+		    if [[ "$pull" =~ "Arduino/printy.ino/" ]]; then
+                echo Arduino source code changed
+                cloned=1
+            fi
+            if [[ "$pull" =~ "Raspberry/" ]]; then
+                echo Arduino source code changed
+                cloned=$((cloned+2))
+            fi
+		    # Otherwise if some non-Arduino files changed
+		    if [[ "$cloned" -eq 0 ]]; then
+			    echo Updated non-Arduino files
+			    cloned=-2
+		    fi
 		fi
 	# Otherwise retry
 	else
@@ -98,25 +109,32 @@ done
 cd /home/pi
 compiled=-1
 
-# Compile if a new version is available, if it's the first time the code is compiled or if last compile was unsuccessfull
-if [ "$cloned" -eq 1 ] || [ "`cat /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile`" -ne "0" ]; then
-	echo "3" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile
-	/home/pi/bin/arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328old Printy-McPrintface/Arduino/printy.ino
-	echo "$?" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile
-	compiled=$?
-fi
+if [[ "$cloned" -gt 0 ]]; then
+    # Compile if a new version is available, if it's the first time the code is compiled or if last compile was unsuccessfull
+    if [[ "$cloned%2" -eq 1 ]] || [[ "`cat /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile`" -ne "0" ]]; then
+        echo "3" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile
+        /home/pi/bin/arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328old Printy-McPrintface/Arduino/printy.ino
+        echo "$?" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastCompile
+        compiled=$?
+    fi
 
-# Upload if a new version is available, if it's the first time the code is uploaded or if last upload was unsuccessfull
-if [ "$compiled" -eq 0 ] || [ "`cat /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload`" -ne "0" ]; then
-	echo "3" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload
-	sleepTime=0
-	touch /home/pi/Printy-McPrintface/Raspberry/git-pull/.updating
-	while [ -f "/home/pi/Printy-McPrintface/Raspberry/controller-ino/.serialOpen" ] && [ "$sleepTime" -lt 60 ]
-	do
-		sleep 1
-		sleepTime=$(($sleepTime+1))
-	done
-	/home/pi/bin/arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328old Printy-McPrintface/Arduino/printy.ino
-	echo "$?" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload
-	rm /home/pi/Printy-McPrintface/Raspberry/git-pull/.updating
+    # Upload if a new version is available, if it's the first time the code is uploaded or if last upload was unsuccessfull
+    if [[ "$compiled" -eq 0 ]] || [[ "`cat /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload`" -ne "0" ]]; then
+        echo "3" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload
+        sleepTime=0
+        touch /home/pi/Printy-McPrintface/Raspberry/git-pull/.updating
+        while [[ -f "/home/pi/Printy-McPrintface/Raspberry/controller-ino/.serialOpen" ]] && [[ "$sleepTime" -lt 60 ]]
+        do
+            sleep 1
+            sleepTime=$(($sleepTime+1))
+        done
+        /home/pi/bin/arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328old Printy-McPrintface/Arduino/printy.ino
+        echo "$?" > /home/pi/Printy-McPrintface/Raspberry/git-pull/.lastUpload
+        rm /home/pi/Printy-McPrintface/Raspberry/git-pull/.updating
+    fi
+
+    cloned=$((cloned>>1))
+    if [[ "$cloned%2" -eq 1 ]]; then
+        sudo reboot
+    fi
 fi
